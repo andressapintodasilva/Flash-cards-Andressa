c72deda5b3674fea29a3b6577b6393bea742615f
andressapintodasilva <146196237+andressapintodasilva@users.noreply.github.com>
2024-11-21 14:32:23 -0300

    21/11

--- perguntas.js
+++ perguntas.js
@@ -4,16 +4,19 @@ criaCartao(
     'A que temperatura a água ferve?',
     '100° C'
 )
+
 criaCartao(
     'Geografia',
     'Qual a maior floresta tropical do mundo?',
     'Floresta Amazônica'
 )
+
 criaCartao(
     'Biologia',
     'O que os pandas comem?',
     'Bambu'
 )
+
 criaCartao(
     'Biologia',
     'Que fruto nasce da oliveira?',
@@ -21,6 +24,6 @@ criaCartao(
 
 
 )
-    
-    
-    
+
+
+
